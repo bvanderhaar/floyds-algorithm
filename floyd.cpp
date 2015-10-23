a1cb0c5d169a0958c7b57e68dbcd94605dc8f6bc
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-10-22 22:25:21 -0400

    proper floyd implementation

--- floyd.cpp
+++ floyd.cpp
@@ -15,59 +15,94 @@ std::vector<int> slurp(const std::string &filename) {
   std::stringstream sstr;
   sstr << in.rdbuf();
   while (sstr >> i) {
-    std::cout << "read i: " << i << std::endl;
+    // std::cout << "read i: " << i << std::endl;
     vect.push_back(i);
     if (sstr.peek() == '\t' || sstr.peek() == ' ') {
       sstr.ignore();
-      }
+    }
   }
   return vect;
 }
 
-int main(int argc, char *argv[]) {
-  int i, j, k=0;
-  if (argc != 2) {
-    std::cout << "Usage: floyds-algorithm num_vertices" << std::endl;
-  }
-  std::vector<int> flat_array = slurp("matrix.out");
-  int vertices = atoi(argv[1]);
-  int graph[vertices][vertices];
+void print_2d_array(std::vector<std::vector<int>> array, int vertices) {
+  int i = 0, j = 0;
   for (i = 0; i < vertices; i++) {
     for (j = 0; j < vertices; j++) {
-      std::cout << "i: " << i << " j: " << j << " value: " << flat_array[k] << std::endl;
-      graph[i][j] = flat_array[k];
-      k++;
+      if (array[i][j] == INF) {
+        std::cout << "INF"
+                  << "\t";
+      } else {
+        std::cout << array[i][j] << "\t";
+      }
+    }
+    std::cout << "\n";
+  }
+}
+
+std::vector<std::vector<int>>
+calc_distance(std::vector<std::vector<int>> distance_matrix, int vertices) {
+  std::vector<std::vector<int>> solution;
+  int i = 0, j = 0, k = 0, normalized;
+  solution.resize(vertices, std::vector<int>(vertices, INF));
+
+  // normalize the array with INF to prevent arithmetic overflow
+  for (i = 0; i < vertices; i++) {
+    for (j = 0; j < vertices; j++) {
+      normalized = distance_matrix[i][j];
+      if (normalized > INF) {
+        normalized = INF;
+      }
+      solution[i][j] = normalized;
     }
   }
 
-  /* dist[][] will be the output matrix that will finally have the shortest
-    distances between every pair of vertices */
-  int dist[vertices][vertices];
-
-  /* Initialize the solution matrix same as input graph matrix. Or
-     we can say the initial values of shortest distances are based
-     on shortest paths considering no intermediate vertex. */
-  for (i = 0; i < vertices; i++)
-    for (j = 0; j < vertices; j++)
-      dist[i][j] = graph[i][j];
-
-  /* Add all vertices one by one to the set of intermediate vertices.
-    ---> Before start of a iteration, we have shortest distances between all
-    pairs of vertices such that the shortest distances consider only the
-    vertices in set {0, 1, 2, .. k-1} as intermediate vertices.
-    ----> After the end of a iteration, vertex no. k is added to the set of
-    intermediate vertices and the set becomes {0, 1, 2, .. k} */
+  //std::cout << "normalized array: \n" << std::endl;
+  //print_2d_array(solution, vertices);
   for (k = 0; k < vertices; k++) {
-    // Pick all vertices as source one by one
     for (i = 0; i < vertices; i++) {
-      // Pick all vertices as destination for the
-      // above picked source
       for (j = 0; j < vertices; j++) {
-        // If vertex k is on the shortest path from
-        // i to j, then update the value of dist[i][j]
-        if (dist[i][k] + dist[k][j] < dist[i][j])
-          dist[i][j] = dist[i][k] + dist[k][j];
+        if (solution[i][k] + solution[k][j] < solution[i][j])
+          solution[i][j] = solution[i][k] + solution[k][j];
       }
     }
   }
+  return solution;
+}
+
+std::vector<std::vector<int>> test_graph() {
+  std::vector<std::vector<int>> test_graph;
+  test_graph.resize(4, std::vector<int>(4, INF));
+  test_graph = {
+      {0, 5, INF, 10}, {INF, 0, 3, INF}, {INF, INF, 0, 1}, {INF, INF, INF, 0}};
+  return test_graph;
+}
+
+int main(int argc, char *argv[]) {
+  int vertices = 0, i = 0, j = 0, k = 0;
+  std::vector<std::vector<int>> graph;
+  if (argc < 2) {
+    std::cout << "Usage: floyds-algorithm num_vertices [-t]" << std::endl;
+  }
+  vertices = atoi(argv[1]);
+  std::vector<int> flat_array = slurp("matrix.out");
+  if (argc != 3) {
+    graph.resize(vertices, std::vector<int>(vertices, INF));
+    for (i = 0; i < vertices; i++) {
+      for (j = 0; j < vertices; j++) {
+        // std::cout << "i: " << i << " j: " << j << " value: " << flat_array[k]
+        // << std::endl;
+        graph[i][j] = flat_array[k];
+        k++;
+      }
+    }
+  } else {
+    std::cout << "using test mode\n";
+    vertices = 4;
+    graph = test_graph();
+  }
+
+  std::vector<std::vector<int>> solution = calc_distance(graph, vertices);
+  std::cout << "solution array: " << std::endl;
+  print_2d_array(solution, vertices);
+  return 0;
 }
