27186d0f4275c8dc465e623a0606e8348eedfd1b
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-10-26 15:41:12 -0400

    migrate to gcc

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-clang-format -i floyd.cpp
-clang++ -std=c++14 -O2 floyd.cpp -o floyds-algorithm
+g++ -std=c++14 -fopenmp -O2 floyd.cpp -o floyds-algorithm
 chmod +x floyds-algorithm
-clang++ -std=c++14 -O2 gen_adj_matrix.cpp -o generate-matrix
+g++ -std=c++14 -O2 gen_adj_matrix.cpp -o generate-matrix
 chmod +x generate-matrix
